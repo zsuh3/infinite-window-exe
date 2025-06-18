12e870438ecd3d4f3a18974d58ffcbe02fd828cd
zsuh3 <zionsuh3@gmail.com>
2025-06-18 16:38:53 +1200

    feat: centre image

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 1024,
-    height: 800,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: true
-    }
-  });
-  win.loadFile('index.html');
+    const win = new BrowserWindow({
+        width: 1024,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true
+        }
+    });
+    win.loadFile('index.html');
 }
 
 app.whenReady().then(createWindow);
